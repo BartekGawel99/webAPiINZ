ccf824bb74a1a6520b09aeedab44876178a35664
Bartek Gaweł <bartekgawel99@gmail.com>
2022-12-15 15:21:44 +0100

    azure

--- Program.cs
+++ Program.cs
@@ -13,18 +13,18 @@ builder.Services.AddDbContext<In¿Context>(options =>
         options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 
-builder.WebHost.ConfigureKestrel(options => options.Listen(System.Net.IPAddress.Parse("192.168.0.221"), 5016));
+//builder.WebHost.ConfigureKestrel(options => options.Listen(System.Net.IPAddress.Parse("192.168.0.221"), 5016));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseSwagger();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    
     app.UseSwaggerUI();
 }
 
